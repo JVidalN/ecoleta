1648139e89d21eb895982d090a0b5dfecf433a5b
JVidalN <j.vidalnunes@gmail.com>
2020-06-07 03:10:12 -0300

    Inclusão da busca dos dados pelo servidor. COnfiguração do mapa ainda inacabada.

--- server/src/controllers/itemsController.ts
+++ server/src/controllers/itemsController.ts
@@ -1,24 +1,20 @@
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class ItemsController {
-  
-  async index(request: Request, response: Response){
-    
+  async index(request: Request, response: Response) {
     const items = await knex('items').select('*');
 
-    const serializeditems = items.map(item => {
+    const serializeditems = items.map((item) => {
       return {
         id: item.id,
         title: item.title,
-        image_url: `http://localhost:3333/uploads/${item.image}`,
+        image_url: `http://192.168.2.114:3333/uploads/${item.image}`,
       };
     });
 
     return response.json(serializeditems);
   }
-
 }
 
 export default ItemsController;
-
