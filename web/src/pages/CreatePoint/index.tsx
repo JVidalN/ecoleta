a182b27738b78038c49e50bda4b66eeac637710b
JVidalN <j.vidalnunes@gmail.com>
2020-06-13 16:51:55 -0300

    Included uploadfile with dropzone.

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -7,6 +7,7 @@ import { Map, TileLayer, Marker } from 'react-leaflet';
 import axios from 'axios';
 import { LeafletMouseEvent } from 'leaflet';
 import api from '../../services/api';
+import Dropzone from '../../components/Dropzone';
 
 interface Item {
   id: number;
@@ -34,6 +35,7 @@ const CreatePoint = () => {
   const [selectedCidade, setSelectedCidade] = useState('0');
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const history = useHistory();
 
@@ -111,7 +113,19 @@ const CreatePoint = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = { name, email, whatsapp, latitude, longitude, uf, city, items };
+    //const data = { name, email, whatsapp, latitude, longitude, uf, city, items };
+
+    const data = new FormData();
+
+    data.append('name', name);
+    data.append('email', email);
+    data.append('whatsapp', whatsapp);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('uf', uf);
+    data.append('city', city);
+    data.append('items', items.join(','));
+    if (selectedFile) data.append('image', selectedFile);
 
     await api.post('points', data);
 
@@ -133,6 +147,7 @@ const CreatePoint = () => {
         <h1>
           Cadastro do <br /> ponto de coleta
         </h1>
+        <Dropzone onFileUploaded={setSelectedFile} />
         <fieldset>
           <legend>
             <h2>Dados</h2>
@@ -153,7 +168,6 @@ const CreatePoint = () => {
             </div>
           </div>
         </fieldset>
-
         <fieldset>
           <legend>
             <h2>Endereços</h2>
@@ -198,7 +212,6 @@ const CreatePoint = () => {
             </div>
           </div>
         </fieldset>
-
         <fieldset>
           <legend>
             <h2>Itens de coleta</h2>
@@ -217,7 +230,6 @@ const CreatePoint = () => {
             ))}
           </ul>
         </fieldset>
-
         <button type='submit'>Cadastrar ponto de coleta</button>
       </form>
     </div>
